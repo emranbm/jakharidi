08aa87b613e0f21ce4982b5314936f5bbed98ff2
Emran BatmanGhelich <emran.bm@gmail.com>
2021-11-04 20:16:27 +0330

    Init

---
+++ src/main/java/ir/jakharidi/jakharidi/IndexController.java
@@ -0,0 +1,12 @@
+package ir.jakharidi.jakharidi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "Some awesome API is gonna be born here!\n";
+    }
+}
