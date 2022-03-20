d84c727a40d316e770ca0cbccb461cdc326c4436
Emran Batmanghelich <emran.bm@gmail.com>
2022-03-20 23:02:09 +0330

    Build[back]: Switch to Maven + Add Hibernate

--- backend/src/main/java/ir/whentomeet/backend/controllers/IndexController.java
+++ backend/src/main/java/ir/whentomeet/backend/controllers/IndexController.java
@@ -1,5 +1,4 @@
-package ir.whentomeet.controllers;
-
+package ir.whentomeet.backend.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- backend/src/test/java/ir/whentomeet/backend/BackendApplicationSmokeTests.java
+++ backend/src/test/java/ir/whentomeet/backend/BackendApplicationSmokeTests.java
@@ -1,10 +1,10 @@
-package ir.whentomeet;
+package ir.whentomeet.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class BackendApplicationSmokeTests {
 
 	@Test
 	void contextLoads() {
